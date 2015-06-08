a911e15ffbbfa69d6313cf6c620a3a44933220f6
David Easter <this@davideaster.de>
2015-06-07 23:25:50 -0400

    React app template with AMD modules and ES6 transpiler.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 require( [ "config" ], function() {
-  require( [ "es6!lib/main" ] );
+  require( [ "es6!lib/main", "bower/react/react.min" ], function( App, React ) {
+      var content = document.createElement( "div" );
+      document.body.appendChild( content );
+      React.render( React.createElement( App ), content );
+  } );
 } );
